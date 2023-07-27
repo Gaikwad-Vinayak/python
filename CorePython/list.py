950ead43fb4c5b61cbf3f8af3a497024e3050f6b
vinayak <gaikwadvinayak924@gmail.com>
2023-07-27 23:50:08 +0530

    new chnages

--- CorePython/list.py
+++ CorePython/list.py
@@ -1,5 +1,6 @@
 # list properties --> list defined as carlibresses  
 # append,insert,remove,pop,sort,extend index,count,copy,resurve,sort,
+#  this is list and new add line for git testing
 list=[1,2,3,4,5,6,7]
 list.append(2)
 print(list)
